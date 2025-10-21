967e9466b1cc79ff319aa629b95dc86b8711afac
HisseinHD <hisseindaoud6627@gmail.com>
2025-10-21 11:44:35 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'toumai-frontend_collabo',
+  webDir: 'www'
+};
+
+export default config;
